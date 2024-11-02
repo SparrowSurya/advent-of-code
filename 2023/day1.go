5f4ee41b80ff428fe4672d33c8d8798694067e5c
sparrowsurya <thakur.suryakant.0406@gmail.com>
2024-11-02 12:47:01 +0530

    2023 day1

---
+++ 2023/day1.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"log"
+	"os"
+)
+
+func calibrationValue(text string) uint {
+	var first, last rune
+	for _, ch := range text {
+		if '0' <= ch && ch <= '9' {
+			if first == '\u0000' {
+				first = ch
+			} else {
+				last = ch
+			}
+		}
+	}
+
+	if last == '\u0000' {
+		last = first
+	}
+	
+	return uint(first - '0') * 10 + uint(last - '0')
+}
+
+func solve(lines []string) uint {
+	var count uint
+	
+	for _, line := range lines {
+		count += calibrationValue(line)
+	}
+	
+	return count
+}
+
+func main() {
+	file, err := os.Open("input1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	ans := solve(lines)
+	fmt.Println(ans)
+}
