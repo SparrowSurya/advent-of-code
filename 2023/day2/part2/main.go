a77f2c9db6c7ca402bc050203e79ec98ade3f49f
sparrowsurya <thakur.suryakant.0406@gmail.com>
2024-11-02 17:25:02 +0530

    2023 day2 part2

---
+++ 2023/day2/part2/main.go
@@ -0,0 +1,93 @@
+package main 
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"bufio"
+	"strconv"
+	"strings"
+)
+
+type Game struct {
+	id uint
+	cubes []Cubes
+}
+
+type Cubes struct {
+	red uint
+	green uint
+	blue uint
+}
+
+
+func solve(games []Game) uint {
+	var ans uint
+
+	for _, game := range games {
+		var r,g,b uint
+		for _, cube := range game.cubes {
+			if cube.red > r {
+				r = cube.red
+			}
+			if cube.green > g {
+				g = cube.green
+			}
+			if cube.blue > b {
+				b = cube.blue
+			}
+		}
+		ans += r*g*b 
+	}
+
+	return ans
+}
+
+func parse(file *os.File) []Game {
+	var games []Game
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var slices []string = strings.Split(scanner.Text(), ": ")
+		
+		gid, _ := strconv.Atoi(slices[0][5:])
+
+		var cubes []Cubes
+		for _, rounds := range strings.Split(slices[1], "; ") {
+			
+			var r,g,b uint
+			for _, dice := range strings.Split(rounds, ", ") {
+				pairs := strings.Split(dice, " ") 
+				count, _ := strconv.Atoi(pairs[0])
+				switch pairs[1] {
+					case "red":
+						r += uint(count)
+					case "green":
+						g += uint(count)
+					case "blue":
+						b += uint(count)
+				}
+			}
+			cubes = append(cubes, Cubes{r, g, b})
+		}
+		games = append(games, Game{uint(gid), cubes})
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return games
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var games []Game = parse(file)
+
+	ans := solve(games)
+	fmt.Println(ans)
+}
